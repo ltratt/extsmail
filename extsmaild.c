98c6bc6959212b2a2c9e8803deffc669bcb90600
Laurence Tratt <laurie@tratt.net>
2009-04-27 19:41:20 +0100

    Ensure that we don't try and free from a possibly uninitialised variable.
    Spotted by LLVM's Static Analyzer.

--- extsmaild.c
+++ extsmaild.c
@@ -445,7 +445,7 @@ bool try_groups(Conf *conf, Group *groups, const char *msg_path, int fd)
     off_t mf_body_off = lseek(fd, 0, SEEK_CUR);
     if (mf_body_off == -1) {
         syslog(LOG_ERR, "Error when ftell'ing from '%s'", msg_path);
-        goto fail;
+        goto lseekfail;
     }
 
     // Read in the messages header, doctoring it along the way to make it
@@ -800,11 +800,12 @@ next:
     }
 
 fail:
+    free(dhd_buf);
+lseekfail:
     for (int j = 0; j < nargv; j += 1)
         free(argv[j]);
     free(argv);
     free(stderr_buf);
-    free(dhd_buf);
 
     return false;
 }
