88cc43ddc1520dba5108283f3bedbd288dd515b5
Olivier Girondel <olivier@biniou.info>
2014-05-04 02:54:42 +0200

    Correctly set non-blocking flag.

--- extsmaild.c
+++ extsmaild.c
@@ -1260,7 +1260,7 @@ bool set_nonblock(int fd)
     int flags = fcntl(fd, F_GETFL, 0);
     if (flags == -1)
         return false;
-    if (fcntl(fd, F_SETFL, flags || O_NONBLOCK) == -1)
+    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1)
         return false;
     return true;
 }
